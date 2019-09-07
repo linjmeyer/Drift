5bf95e05bd344e83701a54f6372acf1f685192c0
Lin Meyer <lin.meyer@redbox.com>
2019-09-07 18:22:31 -0500

    Added passed argument to user evals

--- src/CommandLine/UserCodeHelpers.cs
+++ src/CommandLine/UserCodeHelpers.cs
@@ -5,12 +5,12 @@ public static class UserCodeHelpers
     public static T Evaluate<T>(this IDriftStep step, string userCode) 
     {
         var allCode = $@"
-        public {typeof(T)} Evaluate()
+        public {typeof(T)} Evaluate(dynamic context)
         {{
             {userCode}
         }}";
         dynamic script = CSScript.Evaluator.LoadMethod(allCode);
-        return (T) script.Evaluate();
+        return (T) script.Evaluate((dynamic) step);
     }
 
     public static bool EvaluateBool(this IDriftStep step, string userCode)
