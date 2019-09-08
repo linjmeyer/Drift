af2269401962b7da4e8896ae6bebff68e85e73ae
Lin Meyer <lin@linmeyer.net>
2019-09-08 14:56:49 -0500

    Added step: SearchPodStep

--- src/Drift/Steps/GetPodStep.cs
+++ src/Drift/Steps/GetPodStep.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using k8s;
 using k8s.Models;
 using Microsoft.Extensions.Logging;
@@ -8,7 +9,9 @@ namespace Drift.Steps
 {
     public class GetPodStep : AbstractDriftStep
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Namespace { get; set; }
         public V1Pod Pod { get; private set; }
 
